523537646f6d91e08efafc897ebc7c96e697b9d8
Timas101 <timofey.n.silaev@gmail.com>
2025-03-12 20:13:11 +0300

    Update 20807.py

--- KEGE/№13/20807.py
+++ KEGE/№13/20807.py
@@ -4,7 +4,7 @@ net = ip_network('172.16.192.0/255.255.192.0', 0)
 
 count = 0
 for ip in net:
-    if str(bin(ip)).count('1') % 5 != 0:
+    if str(ip).count('1') % 5 != 0:
         count += 1
 
 print(count)
