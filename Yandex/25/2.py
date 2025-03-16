936d148d6e17528b4af21dc474b4d6c4b113eab8
Timas101 <timofey.n.silaev@gmail.com>
2025-03-16 13:07:22 +0300

    Update 2.py

--- Yandex/25/2.py
+++ Yandex/25/2.py
@@ -1,12 +1,14 @@
+from math import sqrt
+
 def DelCheck(num):
     result = set()
-    for i in range(2,num**2):
+    for i in range(2, int(sqrt(num)) + 1):
         if num % i == 0 and i % 10 == 8:
             result.add(i)
             result.add(num//i)
-    return sum(result)
+    return result
 
 for num in range(114578, 114616 +1):
-    sum_del = DelCheck(num)
+    sum_del = sum(DelCheck(num))
     if sum_del % 10 == 6:
         print(num, sum_del)
