4ee071299c13e2999956d6d43d5403d574364785
Timas101 <timofey.n.silaev@gmail.com>
2025-04-18 00:16:37 +0300

    Рар

---
+++ KEGE/24/21421/21421.py
@@ -0,0 +1,44 @@
+alp = [chr(i) for i in range(ord('C'), ord('Z') + 1)]
+alp_ = alp + ['0']
+alp__ = alp + [hex(i)[2:].upper() for i in range(1, 12, 2)]
+base_string = str(open(r"E:\Preparation-to-EGE\KEGE\24\21421\24_21421.txt").readline())
+'''
+string = ''
+for letter in alp:
+    base_string = base_string.replace(letter, ' ')
+string_aray = base_string.split()
+
+max_len = 0
+for str_part in string_aray:
+    while str_part[0] == '0':
+        str_part = str_part[1:] 
+        if len(str_part) == 0:
+            break
+    if str_part:
+        str_part_ten = int(str_part, 12)
+        if str_part_ten % 2 == 0:
+            max_len = max(max_len, len(str_part))
+
+print(max_len)
+'''
+FIND = 1
+COUNT = 0
+
+count = 0
+max_count = 0
+flag = FIND
+for element in reversed(base_string):
+    if flag == FIND:
+        if element in alp__:
+            continue
+        else:
+            i = COUNT
+            count = 1
+    if flag == COUNT:
+        if element in alp_:
+            i = FIND
+            max_count = max(max_count, count)
+            count = 0
+        else:
+            count += 1
+print(max_count)
